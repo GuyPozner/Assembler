3bca434298f72a49dc3cd76439d0dff82e1f3f12
GuyPozner <guy.pozner@mail.huji.ac.il>
2018-08-12 06:04:57 -0400

    added documentation to first_pass.h and second_pass.h

--- src/first_pass.h
+++ src/first_pass.h
@@ -1,6 +1,20 @@
 #ifndef FIRST_PASS_H
 #define FIRST_PASS_H
 
-int * first_pass(char *, symbolTable *);
+/*	Function: first_pass
+ *	-----------------------
+ *	Performs a first pass on the assembly code,
+ *	Saves and assigns adresses to all the symbols,
+ *	Checks code validity, and count the number of 
+ *	code and data lines.
+ *
+ *	@param[in]	filepath Holds the filepath of the input
+ *						 file.
+ *	@param[out]	symbol_table Hold the symbol table to
+ *							 be written in the first pass.
+ *
+ *	@returns: The count of errors detected in first pass.
+ */
+int * first_pass(char *filepath, symbolTable *symbol_table);
 
 #endif
--- src/second_pass.h
+++ src/second_pass.h
@@ -1,6 +1,17 @@
 #ifndef SECOND_PASS_H
 #define SECOND_PASS_H
 
-int second_pass(char *);
+/*	Function: second_pass
+ *	-----------------------
+ *	Performs a second pass on the assembly code,
+ *	writes the .ob, .ent and .ext files if no errors
+ *	were detected in the .as input file.
+ *
+ *	@param[in]	filepath Holds the filepath of the input
+ *						 file.
+ *
+ *	@returns: The count of errors detected in second pass.
+ */
+int second_pass(char *filepath);
 
 #endif
